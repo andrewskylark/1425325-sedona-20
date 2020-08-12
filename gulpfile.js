27bf33d3bc485bb0916374e16bf1a6b525830683
andrewskylark <andrew.skylark@gmail.com>
2020-08-12 19:12:26 +0400

    сборка билда на галп

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,37 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
+const sync = require("browser-sync").create(); // добавляет библиотеку. создает ее
+const csso = require("gulp-csso"); // npm i --save-dev gulp-csso
+const rename = require("gulp-rename");  //npm i --save-dev gulp-rename
+const imagemin = require("gulp-imagemin"); // npm i --save-dev gulp-imagemin
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
-
 const styles = () => {
-  return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
+  return gulp.src("source/sass/style.scss") //взять файл из
+    .pipe(plumber())                       // обработчик ошибок
+    .pipe(sourcemap.init())               // иницализирует карты кода
+    .pipe(sass())                        // sass в css
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(csso())                     // мумифицирует css
+    .pipe(rename("styles.min.css"))    // переименовывает в
+    .pipe(sourcemap.write("."))     // кладет карты кода в корень "."
+    .pipe(gulp.dest("build/css"))  //положить файл в
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
 
 // Server
-
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -40,7 +46,6 @@ const server = (done) => {
 exports.server = server;
 
 // Watcher
-
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
@@ -49,3 +54,66 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// мумификация изображений;
+const images = () => {
+  return gulp.src("sourse/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3})
+    ]))
+}
+exports.images = images;
+
+// добавление фото в webp
+const webpPics = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+}
+
+exports.webp = webpPics; // npx gulp webp
+
+// создает свг спрайт, // npx gulp sprite - выполнить только задачу "sprite"
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite
+
+// коприрование в папку билда
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+    base: "source"  //  копируется вложенность после папки source
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy
+
+// таск удаления содержимого папки build
+const cleanBuild = () => {
+  return del("build/**");
+};
+
+exports.cleanBuild = cleanBuild
+
+// создать build в продакшн
+const build = (done) => gulp.series (
+  "images",
+  "cleanBuild",
+  "copy",
+  "styles",
+  "sprite"
+)
+(done);
+
+exports.build = build
