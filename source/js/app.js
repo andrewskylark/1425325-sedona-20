2c84112d3ca166c26c5696abf0ee8c1e52f45496
andrewskylark <andrew.skylark@gmail.com>
2020-08-20 09:30:41 +0400

    миинификация html js, презагрузка css и шрифта, lazy loading

--- source/js/app.js
+++ source/js/app.js
@@ -39,3 +39,80 @@ function initMap() {
 
   infowindow.open(sedonaMap, marker);
 }
+
+// модальные окна формы
+// кучу переменных потому, что 3 дня извращался - после финальной версии скрипта удалю лишние
+var reviewForm = document.querySelector(".review__form");
+var phone = document.querySelector(".form__input--phone");
+var email = document.querySelector(".form__input--email");
+var inputName = document.querySelector(".form__input--name");
+var surName = document.querySelector(".form__input--surname");
+var modal = document.querySelector(".modal");
+var modalBtns = document.querySelectorAll(".modal__button");
+var modalFail = document.querySelector(".modal--fail");
+var modalShow = document.querySelector(".modal-show");
+var modalSuccess = document.querySelector(".modal--success");
+var closeSuccess = document.querySelector(".modal__button--success");
+var closeFail = document.querySelector(".modal__button--fail");
+var formBtn = document.querySelector(".form__button");
+
+//при незаполненных 4 полях - попап неудачи с фокусом на кнопку
+//закрывается на esc, клик на кнопкку или вне окна
+// иначе - попап удачи с аналогичным поведением, плюс отправка формы при его закрытии
+if (reviewForm) {
+
+  reviewForm.addEventListener("submit", function (evt) {
+    // хотел циклом  с 1 классом required при любом незаполненном, но не получилось
+    if (!inputName.value || !email.value || !phone.value || !surName.value) {
+      evt.preventDefault ();
+      modalFail.classList.add("modal-show");
+      closeFail.focus();
+
+      closeFail.addEventListener ("click", function() {
+      modalFail.classList.remove("modal-show");
+      })
+
+      window.addEventListener ("keydown", function(evt) {
+        if (evt.keyCode === 27) {
+          if (modalFail.classList.contains("modal-show")) {
+              modalFail.classList.remove("modal-show");
+          }
+        }
+      })
+      // при клике во все без класса modal-show
+      window.addEventListener ("click", function(evt) {
+        if (evt.target.classList.contains("modal-show")) {
+        } else {
+          modalFail.classList.remove("modal-show");
+        }
+      })
+
+    } else {
+      evt.preventDefault ();
+      modalSuccess.classList.add("modal-show");
+      closeSuccess.focus();
+
+      closeSuccess.addEventListener ("click", function() {
+        modalFail.classList.remove("modal-show");
+        reviewForm.submit();
+        })
+
+        window.addEventListener ("keydown", function(evt) {
+          if (evt.keyCode === 27) {
+            if (modalSuccess.classList.contains("modal-show")) {
+                modalSuccess.classList.remove("modal-show");
+                reviewForm.submit();
+            }
+          }
+        })
+
+        window.addEventListener ("click", function(evt) {
+          if (evt.target.classList.contains("modal-show")) {
+          } else {
+            modalSuccess.classList.remove("modal-show");
+            reviewForm.submit();
+          }
+        })
+    }
+  })
+};
